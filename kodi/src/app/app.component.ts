f19eb50a4ca5a5f267d30b1466606464374e2f60
Gaspard Mater <gaspard.mater@epitech.eu>
2020-04-29 13:50:58 +0200

    [MOD] project for recived routing and firebase modul

--- kodi/src/app/app.component.ts
+++ kodi/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kodi';
+  constructor() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyCLRUdDfBX1zFgElZeM_Pf6Zx8k7GBLKes",
+        authDomain: "kodi-f66da.firebaseapp.com",
+        databaseURL: "https://kodi-f66da.firebaseio.com",
+        projectId: "kodi-f66da",
+        storageBucket: "kodi-f66da.appspot.com",
+        messagingSenderId: "744009940604",
+        appId: "1:744009940604:web:3b90e7a11bb20fde9053f9",
+        measurementId: "G-K7905DPE8X"
+      };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
--- kodi/src/app/app.module.ts
+++ kodi/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router"
 import { AppComponent } from './app.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatInputModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
